f2fe7beef164ab3b73922d69452cb94779d65db4
Mario Lazzari <mario.lazzari@gmail.com>
2019-09-29 21:01:44 +0200

    Done chapter 2

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,7 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Ciao ciao</div>;
+  const buttonText = "Click me!";
+
+  return (
+    <div>
+      <label htmlFor="name" className="label">
+        Enter name:
+      </label>
+      <input type="text" id="name" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+      <h3>Curernt time:</h3>
+      <h4> {new Date().toLocaleTimeString()}</h4>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
