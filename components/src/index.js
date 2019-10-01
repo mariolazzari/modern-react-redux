04efb800f46f52b1ef80c11a7a997f86f0d661ee
Mario Lazzari <mario.lazzari@gmail.com>
2019-10-01 20:17:16 +0200

    Done chapter 3

--- components/src/index.js
+++ components/src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import ApprovelCard from "./ApprovalCard";
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Mario"
-        time="Today 5:00"
-        content="Nide blog post!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovelCard>
+        <CommentDetail
+          author="Mario"
+          time="Today 5:00"
+          content="Nide blog post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovelCard>
     </div>
   );
 };
