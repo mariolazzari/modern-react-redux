19bbb8b0add64489d3479434635bcb156c32b1b6
Mario Lazzari <mario.lazzari@gmail.com>
2019-10-07 21:03:17 +0200

    Done chaper 16

---
+++ streams/client/src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+
+function App() {
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <Fragment>
+          <Header />
+          <Route path="/" component={StreamList} exact />
+          <Route path="/new" component={StreamCreate} />
+          <Route path="/edit" component={StreamEdit} />
+          <Route path="/delete" component={StreamDelete} />
+          <Route path="/show" component={StreamShow} />
+        </Fragment>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ streams/client/src/components/streams/StreamList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function StreamList() {
+  return <div>stream list</div>;
+}
